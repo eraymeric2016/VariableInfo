8249a96ced2a6b3678135c287881b551c73d758f
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:20:57 +0300

    Add files via upload

---
+++ VariableInfo.java
@@ -0,0 +1,105 @@
+package week_03;
+
+public class VariableInfo {
+    public static void main(String[] args) {
+        /*
+        Elbette, Java'nın 8 temel veri tipi şunlardır:
+
+byte: 8 bit uzunluğunda ve -128 ila 127 arasında tamsayı değerlerini tutar.
+
+short: 16 bit uzunluğunda ve -32,768 ila 32,767 arasında tamsayı değerlerini tutar.
+
+int: 32 bit uzunluğunda ve -2.147.483.648 ila 2.147.483.647 arasında tamsayı değerlerini tutar.
+
+long: 64 bit uzunluğunda ve -9.223.372.036.854.775.808 ila 9.223.372.036.854.775.807 arasında tamsayı değerlerini tutar.
+
+float: 32 bit uzunluğunda ve ondalıklı sayı değerlerini tutar.
+
+double: 64 bit uzunluğunda ve ondalıklı sayı değerlerini tutar.
+
+char: 16 bit uzunluğunda ve Unicode karakterlerini tutar.
+
+boolean: yalnızca "true" veya "false" değerlerini tutar.
+
+Bu temel veri tipleri, Java programlamasında çok sık kullanılırlar ve
+her biri belirli bir veri türünü temsil ederler. Verilerin doğru şekilde depolanmasını ve
+işlenmesini sağlamak için, doğru veri türünün seçilmesi önemlidir.
+         */
+
+        double pi = 3.15;
+        double num = 10;
+
+        float floatVar = 12.5f;
+        float floatVar2=(float) 12.5;
+
+        long l=1965411410;
+        long j=111-111-111-1111;
+
+        int i=10;
+
+        short s=9;
+
+        byte b=127;
+
+        char ch='½';
+        char ch1='?';
+
+        boolean bool=true;
+        boolean booll=false;
+
+
+
+
+
+        /*
+        2-non-primitive
+        Merhaba AI bana non-primitive java da ne işe yarardığını basitçe anlatabiirsen çok mutlu olurum. Teşekkürler.
+
+Tabii, memnuniyetle yardımcı olurum!
+
+Java'da, verileri temsil etmek için iki tür veri tipi vardır: primitive (ilkel) ve non-primitive
+(ilkel olmayan). İlkel veri tipleri, basit verileri temsil etmek için kullanılırken,
+ilkel olmayan veri tipleri daha karmaşık veri yapılarını temsil etmek için kullanılır.
+
+Non-primitive veri tipleri, sınıflar veya nesneler olarak adlandırılan yapıları kullanır.
+Bu nesneler, ilkel veri tiplerine göre daha büyük ve karmaşıktır, ancak daha fazla işlevsellik sunarlar.
+
+Örneğin, String sınıfı bir non-primitive veri tipidir ve metinleri temsil etmek için kullanılır.
+Bu sınıf, metin işleme işlevleri sunar ve metinleri birleştirme, ayırma, arama ve
+değiştirme işlemlerini yapmak gibi çeşitli işlemler yapmak için kullanılabilir.
+
+Benzer şekilde, ArrayList sınıfı da bir non-primitive veri tipidir
+ve birden çok değer içeren veri yapılarını temsil etmek için kullanılır.
+Bu sınıf, verileri ekleme, silme, güncelleme ve sıralama işlemlerini yapmak için kullanılabilir.
+
+HashMap sınıfı ise anahtar-değer çiftlerini saklamak için kullanılır
+ve veri yapılarının hızlı bir şekilde aranmasına olanak tanır.
+
+Bu non-primitive veri tipleri, ilkel veri tiplerine göre daha karmaşık olsalar da,
+daha fazla işlevsellik sunarlar ve uygulama geliştirmede çok önemli bir rol oynarlar.
+         */
+
+        String st= "Adam";
+
+        double dd=10;
+        long ll=10;
+        int ii=10;
+        short ss=10;
+
+        dd=ii;
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
